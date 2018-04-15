283e100acb12519daa70edbf28b13cfb482badae
Alexander Antishkin <ighost99@gmail.com>
2018-04-15 13:49:48 +0300

    Window opening

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,10 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
 #include "../include/game.h"
 
 using std::cout;
 using std::endl;
 
 int main(/*int argc, const char** argv*/) {
-    cout << "Hello, world" << endl;
-    game_map a("/home/darya/Downloads/test-map");
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "MAD");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
 }
